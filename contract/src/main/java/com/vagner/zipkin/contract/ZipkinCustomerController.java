5f6948d2771375d315ffc30672539a355e4bfe5e
Vagner Nascimento <vagnernasc88@gmail.com>
2018-11-20 17:45:51 -0200

    Implementation completed, working perfectly

--- contract/src/main/java/com/vagner/zipkin/contract/ZipkinCustomerController.java
+++ contract/src/main/java/com/vagner/zipkin/contract/ZipkinCustomerController.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Slf4j
@@ -18,10 +16,8 @@ public class ZipkinCustomerController {
     @Autowired
     private ZipkinCustomerFacade facade;
 
-    @GetMapping(value = "salaries/{function}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<DeveloperResponse> getCustomerSalariesByFunction(@PathVariable("function")
-                                                                 @NotNull(message = "Customer's function must be provided")
-                                                                         String function) {
-        return facade.getCustomerSalariesByFunction(function);
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DeveloperResponse> getCustomerSalariesByFunction() {
+        return facade.getCustomerSalariesByFunction();
     }
 }
